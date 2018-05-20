aab859dc71209748032824500ab823a188374fc1
Adrian Marino <adrianmarino@gmail.com>
2018-05-19 21:16:30 -0300

    add UMG module

--- Source/PuzzlePlatforms/PuzzlePlatforms.Build.cs
+++ Source/PuzzlePlatforms/PuzzlePlatforms.Build.cs
@@ -6,6 +6,8 @@ public class PuzzlePlatforms : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "UMG"
+		});
 	}
 }
